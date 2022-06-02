7b9e6c0ae4ec5a12259f5e1576220dba0b1c19fb
Ilya Gagarin <ilyaspace25@gmail.com>
2022-06-02 15:54:31 +0300

    commit

--- RubymineProjects/studyP/RGZ/RGZ.rb
+++ RubymineProjects/studyP/RGZ/RGZ.rb
@@ -1,9 +1,37 @@
 numbers = Array[1,2,3,4,5,6,7,8,9,10]
 
-def even_first_odd_second (numbers)
-  i = 0
-end
-  for i to numbers
-    if i 
+def even_first_odd_second (ary)
+  if ary.length % 2 == 0
+  odd = Array.new(ary.length/2)
+  else
+    odd = Array.new((ary.length/2)-1)
   end
+  for i in 0..ary.length do
+    if i % 2 == 0
+      puts ary[i]
+      puts ""
+    else
+      odd[i-1] = ary[i]
+    end
+  end
+  puts odd
+  puts ""
 end
+
+def odd_first_even_second (arr)
+  even = Array.new(arr.length/2)
+  for i in 0..arr.length do
+    if i % 2 == 0
+      puts arr[i]
+      puts ""
+    else
+      even[i-1] = arr[i]
+    end
+  end
+  puts even
+  puts ""
+end
+
+even_first_odd_second(numbers)
+odd_first_even_second(numbers)
+
